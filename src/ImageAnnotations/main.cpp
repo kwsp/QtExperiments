a492ddebe3ecd720a1595dcba91ec2b30130e981
tnie <tnie@tuta.io>
2024-06-03 12:48:25 -0500

    refactor

--- src/ImageAnnotations/main.cpp
+++ src/ImageAnnotations/main.cpp
@@ -1,45 +1,54 @@
 #include <QApplication>
-#include <QGraphicsView>
-#include <QGraphicsScene>
 #include <QGraphicsPixmapItem>
+#include <QGraphicsScene>
+#include <QGraphicsView>
 #include <QVBoxLayout>
 #include <QWidget>
+#include <qgraphicsitem.h>
+#include <qnamespace.h>
 
 #include "LineAnnotation.hpp"
 
 class AnnotationView : public QGraphicsView {
 public:
-    explicit AnnotationView(QWidget *parent = nullptr) : QGraphicsView(parent) {
-        setRenderHint(QPainter::Antialiasing);
-        setRenderHint(QPainter::SmoothPixmapTransform);
-    }
+  explicit AnnotationView(QWidget *parent = nullptr)
+      : QGraphicsView(parent), m_scene(new QGraphicsScene) {
+    setRenderHint(QPainter::Antialiasing);
+    setRenderHint(QPainter::SmoothPixmapTransform);
+
+    setScene(m_scene);
+  }
+
+  auto scene() -> QGraphicsScene * { return m_scene; }
+
+private:
+  QGraphicsScene *m_scene;
 };
 
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
+  QApplication app(argc, argv);
 
-    QWidget mainWindow;
-    QVBoxLayout layout(&mainWindow);
+  QWidget mainWindow;
+  QVBoxLayout layout(&mainWindow);
 
-    auto *view = new AnnotationView;
-    auto *scene = new QGraphicsScene;
+  auto *view = new AnnotationView;
+  auto scene = view->scene();
 
-    // Load and display an image
-    QPixmap image("path/to/your/image.png");
-    auto *imageItem = new QGraphicsPixmapItem(image);
-    scene->addItem(imageItem);
+  // Load and display an image
+  QPixmap image("path/to/your/image.png");
+  auto *imageItem = new QGraphicsPixmapItem(image);
+  scene->addItem(imageItem);
 
-    // Add line and box annotations
-    auto *line = new LineAnnotation(QPointF(50, 50), QPointF(200, 200), Qt::blue);
-    scene->addItem(line);
+  // Add line and box annotations
+  auto *line = new LineAnnotation(QPointF(50, 50), QPointF(200, 200), Qt::blue);
+  scene->addItem(line);
 
-    auto *box = new BoxAnnotation(QRectF(100, 100, 150, 100), Qt::blue);
-    scene->addItem(box);
+  auto *box = new BoxAnnotation(QRectF(100, 100, 150, 100), Qt::blue);
+  scene->addItem(box);
 
-    view->setScene(scene);
-    layout.addWidget(view);
-    mainWindow.setLayout(&layout);
-    mainWindow.show();
+  layout.addWidget(view);
+  mainWindow.setLayout(&layout);
+  mainWindow.show();
 
-    return app.exec();
+  return app.exec();
 }
