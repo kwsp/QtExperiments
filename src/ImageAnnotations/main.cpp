bc1111fc98aaaf51630fc9f5923f1864eeab76ac
tnie <tnie@tuta.io>
2024-06-04 15:26:10 -0500

    support pinch zoom

--- src/ImageAnnotations/main.cpp
+++ src/ImageAnnotations/main.cpp
@@ -1,11 +1,11 @@
 #include <QApplication>
+#include <QGestureEvent>
 #include <QGraphicsPixmapItem>
 #include <QGraphicsScene>
 #include <QGraphicsView>
+#include <QPinchGesture>
 #include <QVBoxLayout>
 #include <QWidget>
-#include <qgraphicsitem.h>
-#include <qnamespace.h>
 
 #include "LineAnnotation.hpp"
 
@@ -13,15 +13,46 @@ class AnnotationView : public QGraphicsView {
 public:
   explicit AnnotationView(QWidget *parent = nullptr)
       : QGraphicsView(parent), m_scene(new QGraphicsScene) {
+    setScene(m_scene);
+
     setRenderHint(QPainter::Antialiasing);
     setRenderHint(QPainter::SmoothPixmapTransform);
 
-    setScene(m_scene);
+    setDragMode(DragMode::ScrollHandDrag);
+    viewport()->setAttribute(Qt::WA_AcceptTouchEvents);
+    grabGesture(Qt::PinchGesture);
   }
 
   auto scene() -> QGraphicsScene * { return m_scene; }
 
+protected:
+  bool event(QEvent *event) override {
+    if (event->type() == QEvent::Gesture) {
+      return gestureEvent(static_cast<QGestureEvent *>(event));
+    }
+    return QGraphicsView::event(event);
+  }
+
 private:
+  bool gestureEvent(QGestureEvent *event) {
+    if (QGesture *pinch = event->gesture(Qt::PinchGesture)) {
+      pinchTriggered(static_cast<QPinchGesture *>(pinch));
+    }
+    return true;
+  }
+
+  void pinchTriggered(QPinchGesture *gesture) {
+    if (gesture->state() == Qt::GestureState::GestureUpdated) {
+      const qreal scaleFactor = gesture->scaleFactor();
+      m_scaleFactor = m_scaleFactor * scaleFactor;
+      QTransform transform;
+      transform.scale(m_scaleFactor, m_scaleFactor);
+      setTransform(transform);
+    }
+  }
+
+  qreal m_scaleFactor{1.0};
+
   QGraphicsScene *m_scene;
 };
 
@@ -32,7 +63,7 @@ int main(int argc, char *argv[]) {
   QVBoxLayout layout(&mainWindow);
 
   auto *view = new AnnotationView;
-  auto scene = view->scene();
+  auto *scene = view->scene();
 
   // Load and display an image
   QPixmap image("path/to/your/image.png");
